e15a0738c960b9eb6ad5d5fd19d529b5860d29b1
Jarod Peachey <jwpeachey107@aol.com>
2020-04-06 17:59:29 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Triangle CMS
+
+Triangle CMS is a modern CMS built on the JAMstack. Welcome to the future of intuitive, powerful and lighting fast website development
---
+++ packages/gatsby-plugin-triangle/gatsby-node.js
@@ -0,0 +1,72 @@
+/**
+ * Implement Gatsby's Node APIs in this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/node-apis/
+ */
+
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
+const fetch = require('node-fetch');
+
+exports.sourceNodes = async ({
+  actions,
+  createNodeId,
+  createContentDigest,
+  reporter,
+}, options) => {
+  const { createNode } = actions;
+
+  const apiKey = options.key;
+  const {siteID} = options;
+
+  if (!apiKey) {
+    reporter.panicOnBuild('Please define a Netlify access token');
+  }
+
+  if (!siteID) {
+    reporter.panicOnBuild('Please define a site ID');
+  }
+
+  // const client = new NetlifyAPI(apiKey, opts);
+
+  const nodeHelper = (input, name) => {
+    // input.netlify_id = input.id;
+    // input.id = createNodeId(`gatsby-source-netlify-${input.netlify_id}`);
+    // console.log(input);
+
+    const nodeMeta = {
+      id: input.id,
+      parent: null,
+      children: [],
+      internal: {
+        type: `Netlify${name}`,
+      },
+    };
+    nodeMeta.internal.content = JSON.stringify(nodeMeta);
+    nodeMeta.internal.contentDigest = createContentDigest(nodeMeta);
+
+    console.log(nodeMeta);
+
+    createNode({ ...input, ...nodeMeta});
+  };
+
+  try {
+    await fetch(
+      `https://api.netlify.com/api/v1/sites/${siteID}/submissions/?access_token=${apiKey}`
+    ).then((res) => {
+      res.json().then((json) => {
+        console.log(typeof json);
+        console.log(json);
+
+        Object.values(json).forEach((submission) => {
+          nodeHelper(submission, 'Submissions');
+        });
+      });
+    });
+  } catch (e) {
+    console.error(e);
+    process.exit(1);
+  }
+};
