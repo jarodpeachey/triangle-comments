0b4011d7f9a1352a67120aaf8ec17128bac76506
Jarod Peachey <jwpeachey107@aol.com>
2020-04-17 22:46:34 -0400

    Test styling updates

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,12 +32,6 @@ module.exports = {
         icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
       },
     },
-    {
-      resolve: 'gatsby-plugin-zopfli',
-      options: {
-        path: 'zopfli',
-      },
-    },
     'gatsby-plugin-styled-components',
   ],
 };
